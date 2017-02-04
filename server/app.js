d83e28876504932e61160fba740f0e4fe3bacd64
Dane <dingchao.lonton@gmail.com>
2017-02-04 13:09:39 +0800

    compress response boy

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var engine = require('consolidate');
+var compression = require('compression');
+
 
 var index = require('./routes/index');
 var apis = require('./routes/apis');
@@ -18,6 +20,7 @@ app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
